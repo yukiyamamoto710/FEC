656512592ffc498ef70fd01231c05f221804d020
hanbokhe <sooyeonk@usc.edu>
2021-06-03 12:45:25 -0700

    QA

---
+++ client/components/QA/QA.jsx
@@ -0,0 +1,15 @@
+import react from 'react';
+import Searchbar from './Searchbar.jsx';
+import QAItem from './QAItem.jsx';
+
+const QA = (props)=> {
+  render () {
+    return (
+      <div>
+        <Searchbar/>
+      </div>
+    );
+  }
+}
+
+export default QA;
