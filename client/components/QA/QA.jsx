cca1d4ef15c4dd8db4b022d734771fc4fe37958b
hanbokhe <sooyeonk@usc.edu>
2021-06-07 13:29:20 -0700

    testing sorted questions

--- client/components/QA/QA.jsx
+++ client/components/QA/QA.jsx
@@ -3,13 +3,36 @@ import QAItem from './QAItem.jsx';
 import AddQ from './AddQ.jsx';
 
 
-const QA = ({questions}) => {
-  return (
-    <div>
-      <h3>Questions and Answers</h3>
-      <AddQ/>
-      {questions.results.map(question => <QAItem question={question} key={question.question_id}/>)}
-    </div>
-  );
+class QA extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    //this.addQuestion = this.addQuestion.bind(this);
+    this.sortQuestion = this.sortQuestion.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({questions: this.props.questions}, () => {sortQuestions()})
+  }
+
+  sortQuestions() {
+    var sortedQuestions = _.sortBy(this.state.questions, (question) => {question.question_helfulness})
+    console.log('sorted list', sortedQuestions.reverse());
+  }
+
+  // addQuestion(event, question){
+  //   this.setState({addQuestion: question}, () => {
+  //     axios.put('/put', uestion)
+  //   })
+  // }
+  render() {
+    return (
+      <div>
+        <h3>Questions and Answers</h3>
+        <AddQ/>
+        {this.state.questions.results.map(question => <QAItem question={question} key={question.question_id}/>)}
+      </div>
+    )
+  }
 }
 export default QA;
