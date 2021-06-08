3f7b556d457123e238c9504a59c6091c3d5209d1
Yuki Yamamoto <yukiyamamoto710@gmail.com>
2021-06-07 17:42:19 -0700

    fixed bugs on the Price component

--- client/components/RelatedItems/Price.jsx
+++ client/components/RelatedItems/Price.jsx
@@ -4,13 +4,13 @@ const Price = ({product}) => {
   if (product.results[0].sale_price) {
     return (
       <div className="product-price">
-        <span className="discounted-price">${Math.abs(Number(this.props.product.results[0].sale_price))}</span>
-        <span className="original-price">${Math.abs(Number(this.props.product.results[0].original_price))}</span>
+        <span className="discounted-price">${Math.abs(Number(product.results[0].sale_price))}</span>
+        <span className="original-price">${Math.abs(Number(product.results[0].original_price))}</span>
       </div>
     );
   } else {
     return (
-      <div className="product-price">${Math.abs(Number(this.props.product.results[0].original_price))}</div>
+      <div className="product-price">${Math.abs(Number(product.results[0].original_price))}</div>
     );
   }
 }
