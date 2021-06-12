a67ff4e1e7a7191230c6d931409d2f9be6e2ae68
Yuki Yamamoto <yukiyamamoto710@gmail.com>
2021-06-11 20:17:50 -0700

    refactored Outfits component from class to using hooks

--- client/components/RelatedItems/RelatedItems.jsx
+++ client/components/RelatedItems/RelatedItems.jsx
@@ -117,8 +117,8 @@ class RelatedItems extends React.Component {
   }
 
   addToOutfit() {
+    console.log('invoked???')
     var ids = [...this.state.selectedItemsList].map(item=>item.id);
-    console.log(ids);
     if (ids.indexOf(this.state.currentItem.id) === -1) {
       var updated = [this.state.currentItem, ...this.state.selectedItemsList]
       this.setState({
