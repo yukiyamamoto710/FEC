e4b5ad769bef3ca2de1ab7a6ee8c1b96b153471f
kenneth wang <kennethwang0427@gmail.com>
2021-06-15 21:05:04 -0700

    coverage

--- client/components/Reviews/HelpfulMSG/HelpfulMSG.jsx
+++ client/components/Reviews/HelpfulMSG/HelpfulMSG.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 import putHelpfulReview from '../func/putHelpfulReview/putHelpfulReview';
 import putReportReview from '../func/putReportReview/putReportReview';
 
